3e5e95e3d25c88ae7d4ef9cdeb860cdf8dcd3331
Fernando Farji <fernando.farji@gmail.com>
2022-04-24 21:36:27 -0300

    destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,28 +6,27 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
+
   end
 
   def update
     @article = Article.find(params[:id])
-    @article.update(title: params[:article][:title], content: params[:article][:content])
-    render json: @article
+    @article.update(title: params[:article][:title],content: params[:article][:content])
+    redirect_to @article
   end
 
   def new
     @article = Article.new
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-
-  end
-
   def create
    @article = Article.create(title: params[:article][:title], content: params[:article][:content])
    render json: @article
+  end
+
+  def destroy;
+  @article =Article.find(params[:id])
+  @article.destroy
+  redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   get "bienvenida", to: "home#index"
     # Defines the root path route ("/")
     # root "articles#index"
+    root to: "articles#index"
+
   get "articles/new", to: "articles#new"
   get "articles/:id", to: "articles#show"
   get "articles/:id/edit", to: "articles#edit"
 
-  put "/articles/:id", to: "articles#update", as: :article
+  patch "/articles/:id", to: "articles#update", as: :article
   post "articles", to:"articles#create"
+  delete "/articles/:id", to:"articles#destroy"
 end
